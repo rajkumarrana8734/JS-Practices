1d456f2761cf3489ef7e8cffaaaab9c752869f26
rajkumararana8734 <rajkumarrana8734@gmail.com>
2026-07-08 22:15:22 +0530

    understood variable

--- index.js
+++ index.js
@@ -6,3 +6,14 @@ let x=10;
 //varv is a scope of function ts is not tdz(temporal dead zone)
 console.log(count);
 var count=55;
+
+//let and const is filly blocked scoped variable.
+{
+    const b=2;
+    let a="raj";
+}
+
+console.log(b);
+console.log(a);
+
+
