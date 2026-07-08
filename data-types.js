cb011c52328416c92b6419020a9418e749b9eac7
rajkumararana8734 <rajkumarrana8734@gmail.com>
2026-07-08 22:29:30 +0530

    data types

---
+++ data-types.js
@@ -0,0 +1,22 @@
+//coercion type
+console.log(null+1);
+console.log("5"+1);
+console.log("5"-1);
+console.log(true+false);
+
+
+//types check
+console.log(typeof[]);
+console.log(typeof null);
+console.log(typeof 123n)
+
+//turthy and falsy;
+console.log(Boolean(0));
+console.log(Boolean('0'));
+console.log(Boolean([]));
+console.log(Boolean(undefined));
+
+
+//compare 
+console.log(5 == 5);
+console.log(5 === 5);
