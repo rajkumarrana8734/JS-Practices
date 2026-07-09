7a8e2c7eae9b86461577e098d23a998faf366919
rajkumararana8734 <rajkumarrana8734@gmail.com>
2026-07-09 21:50:11 +0530

    operators

---
+++ operaters.js
@@ -0,0 +1,39 @@
+//Arithmetic operators
+let a=5, b=10;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b);
+
+
+//assignment operators
+let score=2;
+score += 2;
+score -=2
+console.log(score);
+
+//compararison operators
+console.log(5=="5");
+console.log(5==="5");
+let value = 12;
+console.log(value > 10); //ture
+
+//logical operators
+let age=20, hasid=true; 
+if(age>=18 && hasid){
+    console.log("allowed");
+}
+
+//unary
+let x = "5";
+console.log(+x); 
+console.log(x++);
+
+//ternary
+let marks=80;
+let grade = marks > 90 ? "pass" : "fail";
+ 
+
+
