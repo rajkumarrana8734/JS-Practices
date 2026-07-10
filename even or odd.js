da371818226fff234b89774ed48a1b5fee03cbda
rajkumararana8734 <rajkumarrana8734@gmail.com>
2026-07-10 21:55:51 +0530

    even or odd number find

--- even or odd.js
+++ even or odd.js
@@ -5,3 +5,15 @@ if (num % 2 === 0) {
 } else {
     console.log(num + " is Odd");
 }
+
+
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+numbers.forEach(function(n) {
+    if (n % 2 === 0) {
+        console.log(n + " -> Even");
+    } else {
+        console.log(n + " -> Odd");
+    }
+});
