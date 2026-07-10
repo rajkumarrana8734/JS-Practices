7f13b42010b27cdca8dbdb517e4b3bcb30b6ffee
rajkumararana8734 <rajkumarrana8734@gmail.com>
2026-07-10 22:00:11 +0530

    positive negative and zero number check

---
+++ number.js
@@ -0,0 +1,9 @@
+let num = -8;
+
+if (num > 0) {
+    console.log(num + " is Positive");
+} else if (num < 0) {
+    console.log(num + " is Negative");
+} else {
+    console.log(num + " is Zero");
+}
