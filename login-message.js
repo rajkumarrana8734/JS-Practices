a058bd5e1d78ef71631b512d056f256e88807094
rajkumararana8734 <rajkumarrana8734@gmail.com>
2026-07-13 20:45:13 +0530

    login messaqge code

---
+++ login-message.js
@@ -0,0 +1,15 @@
+//login massage 
+
+let isloginIn =false ;
+let admin = true;
+
+if (isloginIn){
+    console.log("login successfully")
+}
+else if(admin){
+    console.log("admin login")
+}
+else{
+    console.log("not login")
+}
+
