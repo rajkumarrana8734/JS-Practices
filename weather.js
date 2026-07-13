ce7b671e5781817eda503f13f7989ea9900cc67c
rajkumararana8734 <rajkumarrana8734@gmail.com>
2026-07-13 20:32:46 +0530

    its a weather check

---
+++ weather.js
@@ -0,0 +1,17 @@
+//check weather
+
+let weather= ""
+switch (weather){
+    case "rainy":
+        console.log("it's a rainy day ⛈️");
+        break;
+    case "sunny":
+        console.log("it's a sunny day 🌞");
+        break;
+    case "cloudy":
+        console.log("its a cloudy day ☁️");
+        break;
+        default:
+        console.log("unknown weather");
+        
+}
