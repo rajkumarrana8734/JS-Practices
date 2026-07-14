68e8e0baf1ed971db4304983ede8bf64888e2c6c
rajkumararana8734 <rajkumarrana8734@gmail.com>
2026-07-14 20:24:12 +0530

    while loops

---
+++ while-loops.js
@@ -0,0 +1,24 @@
+//print a number 1 to 5 using while loops
+
+// let i = 1;
+// while (i<6){
+//     console.log(i);
+//     i++;
+// }
+
+
+// let num = 2;
+// let i= 1;
+// while (i<10){
+//     console.log(`${num} x ${i} = ${num * i}`);
+//     i++;
+    
+// }
+
+
+let i = 100;
+
+while (i >= 1) {
+    console.log(i);
+    i--;
+}
