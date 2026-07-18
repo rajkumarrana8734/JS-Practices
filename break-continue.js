8e62fbfe7352e1759d84d2d03d4cb7ed37c45599
rajkumararana8734 <rajkumarrana8734@gmail.com>
2026-07-18 20:36:52 +0530

    break and continue

--- break-continue.js
+++ break-continue.js
@@ -7,12 +7,21 @@ for (let i=1;i<11;i++){
 }
 
 
-
 // Continue 1 to 10 skip 5
 for (let i=1;i<11;i++){
     
     if(i ===5 ){
         continue;
+    }
+    console.log(i)
+}
+
+
+//skip even number
+
+for (let i=0;i<31;i++){
+    if(i%2===0){
+          continue;
     }
     console.log(i)
 }
