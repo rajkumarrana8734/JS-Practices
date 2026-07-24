0d5aa401ad1b388f326ba9dfc7117ee0c4bf0fe7
rajkumararana8734 <rajkumarrana8734@gmail.com>
2026-07-24 20:04:02 +0530

    function practices questions

--- functions.js
+++ functions.js
@@ -1,4 +1,4 @@
-//(1)create a greed function with default function
+// //(1)create a greed function with default function
 
 
 function greed(name="ram"){
@@ -9,7 +9,7 @@ function greed(name="ram"){
 // greed();
 greed("sita");
 
-//(2) sum all num using rest parametrs
+// //(2) sum all num using rest parametrs
 
 function sum(...val){
      let total=0;
@@ -20,7 +20,7 @@ function sum(...val){
 }
 console.log(sum(1,2,3,4,5,6,7,8,9,10));
 
-//(3) create a closure counter function
+// //(3) create a closure counter function
 
 function counter(){
         let count=0;
@@ -37,7 +37,7 @@ console.log(grow());
 console.log(grow());
 console.log(grow());
 
-//(4) create a function then return another function
+// //(4) create a function then return another function
 
 function good(val){
     return function(){
@@ -47,3 +47,61 @@ function good(val){
 
 const like = good("good");
 like();
+
+//(5) use a function to log even numbers in arrays
+
+function even(arr){
+    for ( let num of arr){
+        if (num %2===0){
+            console.log(num);
+        }
+    }
+
+}
+const numbers=[1,2,3,4,5,6,7,8,9,10];
+even(numbers);
+
+//(6) create a pure function to add tax 
+
+function tax(price,taxrate){
+    return price +  (price*taxrate);
+
+}
+
+console.log(tax(20000,0.18));
+console.log(tax(500000,0.20));
+
+// (7) USE iilf function display welcome message
+
+(function (){
+    console.log("Welcome friends");
+}());
+
+//(8) write a disount calculator (hof style)
+
+function disccalculator(discount){
+    return function (price){
+        return price - (price * discount /100);
+    }
+}
+
+const tenpercent = disccalculator(10);
+const twentyprecent = disccalculator(10);
+
+console.log(tenpercent(1000));
+console.log(twentyprecent (900000));
+
+//(9) make a touppercase  transfomer using hof
+
+function transformer(transformFn) {
+    return function (text) {
+        return transformFn(text);
+    };
+}
+
+const toUpper = transformer(function (str) {
+    return str.toUpperCase();
+});
+
+console.log(toUpper("hello"));
+console.log(toUpper("javascript"));
