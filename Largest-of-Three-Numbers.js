6fb25b8676ab7d9881ccd75cec8e6148b254d7b7
rajkumararana8734 <rajkumarrana8734@gmail.com>
2026-07-28 20:42:00 +0530

    Largest-of-Three-Numbers find witch big

---
+++ Largest-of-Three-Numbers.js
@@ -0,0 +1,13 @@
+let a = 92;
+let b = 15;
+let c = 30
+if(a >= b && a>= c){
+    console.log(" A is large");
+}
+else if(b >= a && b>=c ){
+    console.log("B is large");
+}
+else {
+    console.log("C is large");
+}
+
