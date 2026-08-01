f112053f21284554726f1bfdd9542cbde9c5c7a0
rajkumararana8734 <rajkumarrana8734@gmail.com>
2026-08-01 21:32:44 +0530

    user data using objects

---
+++ user-data.js
@@ -0,0 +1,31 @@
+let userData = {
+    name:"sagar patel",
+    age:19,
+    marriedStatus:"single",
+    contact:{
+        PhoneNo:8734980745,
+        email:"rajkumar7756@gmail.com",
+    address:{
+        village:"sayan",
+        city:"surat",
+        state:"gujarat",
+        country:"india",
+        pin:394130,
+    educationdetails:{
+        education:"10th,12th,BCA",
+        collage:"sardar patel university",
+    parentsDetails:{
+        fatherName:"patel rajkumar",
+        motherName:"patel bhavna",
+        fatherOccupation:"farmer",
+        motherOccupation:"housewife",
+        fatherMobileNo:87349423342,
+        motherMobileNo:42424522211
+             }
+          }
+       }
+    }
+}
+
+console.log(userData);
+console.log(userData.contact);
