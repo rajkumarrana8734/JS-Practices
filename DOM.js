c78dfa334024d9ac7977ff2cb044698c434d9d82
rajkumararana8734 <rajkumarrana8734@gmail.com>
2026-08-02 21:18:59 +0530

    dom practice question

---
+++ DOM.js
@@ -0,0 +1,27 @@
+// Ek <h1> ka text "Hello" se "Welcome" karo.
+
+let heading = document.querySelector(".h1");
+heading.textContent = "Welcome";
+console.dir(heading);
+
+// Ek paragraph ka color blue karo.
+
+let paragraph = document.querySelector(".pag");
+paragraph.style.color = "blue";
+console.dir(paragraph);
+
+//Button click par alert dikhao.
+
+let btn = document.querySelector(".btn");
+btn.setAttribute("onclick", "alert('Button clicked!')");
+
+//Button click par background color change karo
+let btn2 = document.querySelector(".btn2");
+btn2.setAttribute("onclick", "document.body.style.backgroundColor = 'green';");
+
+// Ek image ka src change karo.
+
+let img = document.querySelector("img");
+img.src = "https://images.unsplash.com/photo-1779896412017-0583c466e261?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw4fHx8ZW58MHx8fHx8";
+img.style.width = "300px";
+
