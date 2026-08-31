9a27e07fba1e192ee13cc1e4ef508ce7276d977e
rajkumararana8734 <rajkumarrana8734@gmail.com>
2026-08-31 20:55:16 +0530

    BMI calculator js

---
+++ BMICalculator.js
@@ -0,0 +1,18 @@
+
+        function showTime() {
+            let date = new Date();
+
+            let hours = date.getHours();
+            let minutes = date.getMinutes();
+            let seconds = date.getSeconds();
+
+            hours = hours < 10 ? "0" + hours : hours;
+            minutes = minutes < 10 ? "0" + minutes : minutes;
+            seconds = seconds < 10 ? "0" + seconds : seconds;
+
+            document.getElementById("clock").innerText =
+                hours + ":" + minutes + ":" + seconds;
+        }
+
+        showTime();
+        setInterval(showTime, 1000);
